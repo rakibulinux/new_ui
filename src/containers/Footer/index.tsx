e9b7841109a8b7f0f6a77c3092644d0dbfc785b1
cin12211 <56920718+cin12211@users.noreply.github.com>
2021-05-14 12:16:41 +0700

    Featrure : new header footer

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,96 +1,129 @@
 import * as React from 'react';
-import { RouterProps } from 'react-router';
-import { Link, withRouter } from 'react-router-dom';
-//import { LogoIcon } from '../../assets/images/LogoIcon';
+import { useState } from 'react';
 
-const TelegramIcon = require('../../assets/images/landing/social/Telegram.svg');
-//const LinkedInIcon = require('../../assets/images/landing/social/LinkedIn.svg');
-const TwitterIcon = require('../../assets/images/landing/social/Twitter.svg');
-//const YouTubeIcon = require('../../assets/images/landing/social/YouTube.svg');
-//const RedditIcon = require('../../assets/images/landing/social/Reddit.svg');
-/* const FacebookIcon = require('../../assets/images/landing/social/Facebook.svg'); */
-const MediumIcon = require('../../assets/images/landing/social/Medium.svg');
-//const CoinMarketIcon = require('../../assets/images/landing/social/CoinMarket.svg');
+import { useHistory, Link } from 'react-router-dom';
+
+//  import styled from "styled-components";
+import { FaPhoneAlt, FaTelegram, FaTwitter, FaFacebook, FaEnvelope, FaGlobe } from 'react-icons/fa';
+
 const Logo = require('../../assets/images/logo.svg');
-const email = require('./icon/email.svg');
-/* const phone = require('./icon/phone.svg')
-const office = require('./icon/office.svg') */
 
-class FooterComponent extends React.Component<RouterProps> {
-	public render() {
-		if (this.props.history.location.pathname.startsWith('/confirm')) {
-			return <React.Fragment />;
-		}
+export const Footer: React.FC = Props => {
+	let history = useHistory();
 
-		return <div className="display">{this.renderFooterDesktop()}</div>;
+	const [emailAddress, setemailAddress] = useState('');
+
+	const inputEmail = (e: any) => {
+		setemailAddress(e.target.value);
+	};
+
+	const sendEmail = () => {
+		const valueEmail = emailAddress;
+		// do something
+		console.log('đã gửi', valueEmail);
+		setemailAddress('');
+	};
+
+	if (history.location.pathname.startsWith('/confirm')) {
+		return <React.Fragment />;
 	}
 
-	private renderFooterDesktop = () => {
-		return (
-			<footer className="page-footer font-small unique-color-dark">
-				<div className="text-center text-md-left mt-5 hmax">
-					<div className="row">
-						<div className="col-md-3 col-lg-3 col-xl-3 mx-auto">
-							<div className="pg-logo">
-								<a href="/">
-									<img src={Logo} alt="" className="pg-logo__img" />
+	return <React.Fragment>{renderFooterDesktop(inputEmail, sendEmail, emailAddress)}</React.Fragment>;
+};
+
+const renderFooterDesktop = (inputEmail, sendEmail, emailAddress) => {
+	const valueInput: string = emailAddress;
+	return (
+		<div className="footerDesktop-screen">
+			<div className="container-footer-screen">
+				<div className="footer d-flex flex-row justify-content-between ">
+					<div className="footer__logo">
+						<a className="footer__logo__img" href="/">
+							<img src={Logo} alt="" />
+						</a>
+
+						<p className="footer__logo__desc">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, quibusdam nesciunt voluptatem fugit
+							saepe soluta obcaecati et assumenda
+						</p>
+					</div>
+
+					<div className="footer__info">
+						<p className="footer__info__title">CONTACT</p>
+						<p className="footer__info__item">
+							<FaPhoneAlt className="footer__info__item__icon" /> 0905333999
+						</p>
+						<p className="footer__info__item">
+							<FaEnvelope className="footer__info__item__icon" /> liting@cx.finance
+						</p>
+						<p className="footer__info__item footer__info__item--default">
+							<FaGlobe className="footer__info__item__icon" /> www.litingcx.finance
+						</p>
+					</div>
+
+					<div className="footer__info">
+						<p className="footer__info__title">SERVICE SUPPORT</p>
+						<p className="footer__info__item">
+							<Link to="/fee">Asset Fee </Link>
+						</p>
+						<p className="footer__info__item">
+							<Link to="/" target="blank">
+								Listing Token
+							</Link>
+						</p>
+						<p className="footer__info__item">
+							<a href="https://api.cx.finance" target="blank">
+								API Documentation
+							</a>
+						</p>
+					</div>
+
+					<div className="footer__news">
+						<p className="footer__news__title">RECEIVE NEWS</p>
+
+						<div className="footer__news__take-email">
+							<div className="footer__news__take-email__label">Your email</div>
+							<input
+								className="footer__news__take-email__input"
+								placeholder="enter your email"
+								type="email"
+								value={valueInput}
+								onChange={e => inputEmail(e)}
+							/>
+							<span className="footer__news__take-email__btn" onClick={() => sendEmail()}>
+								Send
+							</span>
+						</div>
+
+						<div className="footer__news__list-icon ">
+							<div className="footer__news__list-icon__item  ">
+								<a href="https://twitter.com/exchange_circle">
+									<FaTwitter />
 								</a>
 							</div>
-						</div>
 
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto">
-							<h5 className="text-uppercase font-weight-bold">Service Support</h5>
-							<p>
-								<Link className="footer-lable" to="/fee">
-									Asset Fee
-								</Link>
-							</p>
-							<p>
-								<a href="#" className="footer-lable" target="blank">
-									Listing Token
+							<div className="footer__news__list-icon__item  ">
+								<a href="https://circleex.medium.com/">
+									<FaFacebook />
 								</a>
-							</p>
-							<p>
-								<a href="https://api.cx.finance" className="footer-lable" target="blank">
-									API Documentation
-								</a>
-							</p>
-						</div>
-
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto">
-							<h5 className="text-uppercase font-weight-bold">Contact</h5>
-							<p className="footer-about">
-								<img src={email} alt="Email" />
-								listing@cx.finance
-							</p>
-						</div>
-
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto">
-							<div>
-								<h5 className="text-uppercase font-weight-bold">Social</h5>
-								<a href="https://circleex.medium.com/" className="gplus-ic" target="blank">
-									<img src={MediumIcon} alt="Facebook" />
-								</a>
-								<a href="https://twitter.com/exchange_circle" className="gplus-ic" target="blank">
-									<img src={TwitterIcon} alt="Twitter" />
-								</a>
-								<a href="https://t.me/circleex" className="gplus-ic" target="blank">
-									<img src={TelegramIcon} alt="Telegram" />
+							</div>
+							<div className="footer__news__list-icon__item  ">
+								<a href="https://t.me/circleex">
+									<FaTelegram />
 								</a>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="footer-copyright text-center py-3">
-					© 2020 Copyright:
-					<a href="https://cx.finance/"> cx.finance </a>
-				</div>
-			</footer>
-		);
-	};
-}
+			</div>
 
-// tslint:disable-next-line:no-any
-const Footer = withRouter(FooterComponent as any) as any;
+			<div>
+				<div className="white-line"></div>
 
-export { Footer };
+				<p className="footer__copyright">
+					© 2020 Copyright : <a href="https://cx.finance/"> cx.finance </a>
+				</p>
+			</div>
+		</div>
+	);
+};
