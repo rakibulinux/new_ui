ad474baa56f5ec58f637732832bb33e3d15f983f
nhatduy51 <60657954+nhatduy51@users.noreply.github.com>
2021-05-18 00:00:44 +0700

    market-list

--- src/components/MarketsHotOnList/index.tsx
+++ src/components/MarketsHotOnList/index.tsx
@@ -1,221 +1,230 @@
-import axios from 'axios';
-import * as React from 'react';
+import * as React from "react";
+import axios from "axios";
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Area, AreaChart, ResponsiveContainer } from 'recharts';
+import { useHistory } from "react-router-dom";
 import styled from 'styled-components';
-import {
-	currenciesFetch,
-	Market,
-	selectCurrencies,
-	selectCurrentMarket,
-	selectMarkets,
-	selectMarketTickers,
-	Ticker,
-} from '../../modules';
+import { AreaChart, Area, ResponsiveContainer } from "recharts";
+import { currenciesFetch, selectCurrencies, selectMarkets, selectMarketTickers, Market, selectCurrentMarket, Ticker, setCurrentMarket } from '../../modules';
+
+const ChartWrap = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
+const MarketChartItem = styled.div`
+  min-height: 100px;
+  padding: 10px 0;
+  border-radius: 4px;
+  background-color: var(--tab-panel-background-color);
+  :hover{
+    cursor: pointer;
+    box-shadow: #7d82b8 0px 0px 10px 0px;
+  }
+`;
 
 export const MarketsHotOnlist: React.FC<any> = () => {
-	const ChartWrap = styled.div`
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-	`;
-	const MarketChartItem = styled.div`
-		min-height: 100px;
-		padding: 10px 0;
-		border-radius: 4px;
-		background-color: var(--tab-panel-background-color);
-		transition: all 0.3s;
-		:hover {
-			cursor: pointer;
-			box-shadow: #7d82b8 0px 0px 10px 0px;
-		}
-	`;
-	const defaultTicker = {
-		amount: '0.0',
-		last: '0.0',
-		high: '0.0',
-		open: '0.0',
-		low: '0.0',
-		price_change_percent: '+0.00%',
-		volume: '0.0',
-	};
+    const defaultTicker = {
+        amount: '0.0',
+        last: '0.0',
+        high: '0.0',
+        open: '0.0',
+        low: '0.0',
+        price_change_percent: '+0.00%',
+        volume: '0.0',
+    };
 
-	const marketIds = ['btc/usdt', 'eth/usdt', 'esc/usdt', 'kobe/usdt'];
+    const dispatch = useDispatch();
+    const [marketNames, setMarketNames] = React.useState<string[]>([]);
+    const [kLinesState, setKlinesState] = React.useState<{ pv: string }[]>([]);
 
-	const dispatch = useDispatch();
-	const dispatchFetchCurrencies = () => dispatch(currenciesFetch());
+    const markets = useSelector(selectMarkets);
+    const marketTickers = useSelector(selectMarketTickers);
+    const currencies = useSelector(selectCurrencies);
+    const currentMarket = useSelector(selectCurrentMarket);
 
-	React.useEffect(() => {
-		dispatchFetchCurrencies();
-	}, []);
+    React.useEffect(() => {
+        dispatch(currenciesFetch());
+    }, [])
 
-	const markets = useSelector(selectMarkets);
-	const marketTickers = useSelector(selectMarketTickers);
-	const currencies = useSelector(selectCurrencies);
-	const currentMarket = useSelector(selectCurrentMarket);
+    React.useEffect(() => {
+        if(markets.length){
+            const marketListToState = markets.map((market) => {
+                let price_change_percent = (marketTickers[market.id] || defaultTicker).price_change_percent;
+                let result = 0;
+                if (price_change_percent[0] === "+") {
+                    result = +price_change_percent.split("+").join("").split("%").join("");
+                } else {
+                    result = -price_change_percent.split("-").join("").split("%").join("");
+                }
+                return {
+                    id: market.id,
+                    name : market.name.toLowerCase(),
+                    price_change_percent: result
+                }
+            });
+            const isEmpty = marketListToState.reduce((prev,current)=>{
+                if(current.price_change_percent !== 0){
+                    prev = false;
+                }
+                return prev;
+            },true);
+            if (marketTickers && !marketNames.length && !isEmpty) {
+                marketListToState.sort((a, b) => {
+                    return (b.price_change_percent) - (a.price_change_percent);
+                });
+    
+                const marketNames = marketListToState.slice(0, 3).map(market => {
+                    return market.name
+                });
+                setMarketNames(marketNames);
+            }
+        }
+    }, [marketTickers, markets]);
 
-	//state
-	const [KlineState1, setKline1State] = React.useState<{ pv: string }>();
-	const [KlineState2, setKline2State] = React.useState<{ pv: string }>();
-	const [KlineState3, setKline3State] = React.useState<{ pv: string }>();
-	const [KlineState4, setKline4State] = React.useState<{ pv: string }>();
+    const BASE_MARKET_URL = "https://www.cx.finance/api/v2/peatio/public/markets";
+    const fetchMarketsKlines = async (marketId: string, from: number, to: number) => {
+        try {
+            const klines = await axios.get(`${BASE_MARKET_URL}/${marketId.split('/').join('')}/k-line?period=30&time_from=${from}&time_to=${to}`);
 
-	const BASE_MARKET_URL = 'https://www.cx.finance/api/v2/peatio/public/markets';
-	const fetchMarketsKlines = async (marketId: string, from: number, to: number) => {
-		try {
-			const klines = await axios.get(
-				`${BASE_MARKET_URL}/${marketId.split('/').join('')}/k-line?period=30&time_from=${from}&time_to=${to}`,
-			);
+            return klines.data.map((kline, index) => { return { pv: kline[3] } });
+        } catch (error) {
+            return [];
+        }
+    }
+    React.useEffect(() => {
+        if (marketNames) {
+            const from = Math.floor(Date.now() / 1000) - 60 * 1 * 60 * 1000;
+            const to = Math.floor(Date.now() / 1000);
+            const drawMarketLines = async () => {
+                try {
+                    for (let i = 0; i < marketNames.length; i++) {
+                        const klines = await fetchMarketsKlines(marketNames[i], from, to);
+                        setKlinesState(prev => ([
+                            ...prev,
+                            klines,
+                        ]));
+                    }
+                } catch (error) {
+                    console.log(JSON.stringify(error));
 
-			return klines.data.map((kline, index) => ({ pv: kline[3] }));
-		} catch (error) {
-			return [];
-		}
-	};
-	React.useEffect(() => {
-		const from = Math.floor(Date.now() / 1000) - 60 * 1 * 60 * 1000;
-		const to = Math.floor(Date.now() / 1000);
-		const drawMarketLines = async () => {
-			try {
-				const klines1 = await fetchMarketsKlines(marketIds[0], from, to);
-				const klines2 = await fetchMarketsKlines(marketIds[1], from, to);
-				const klines3 = await fetchMarketsKlines(marketIds[2], from, to);
-				const klines4 = await fetchMarketsKlines(marketIds[3], from, to);
+                }
+                return;
+            }
+            drawMarketLines();
+        }
+    }, [marketNames]);
 
-				setKline1State(klines1);
-				setKline2State(klines2);
-				setKline3State(klines3);
-				setKline4State(klines4);
-			} catch (error) {
-				//console.log(JSON.stringify(error));
-			}
+    const findIcon = (code: string): string => {
+        const currency = currencies.find((currency: any) => String(currency.id).toLowerCase() === code.toLowerCase());
+        try {
+            return require(`../../../node_modules/cryptocurrency-icons/128/color/${code.toLowerCase()}.png`);
+        } catch (err) {
+            if (currency) return currency.icon_url;
+            return require('../../../node_modules/cryptocurrency-icons/svg/color/generic.svg');
+        }
+    };
 
-			return;
-		};
-		// tslint:disable-next-line: no-floating-promises
-		drawMarketLines();
-	}, []);
+    const history = useHistory();
+    const handleRedirectToTrading = (id: string) => {
+        const currentMarket: Market | undefined = markets.find((item) => item.id === id);
 
-	const findIcon = (code: string): string => {
-		const currency = currencies.find(currencyParam => currencyParam.id.toLowerCase() === code.toLowerCase());
-		try {
-			return require(`../../../node_modules/cryptocurrency-icons/128/color/${code.toLowerCase()}.png`);
-		} catch (err) {
-			if (currency) {
-				return currency.icon_url;
-			}
+        if (currentMarket) {
+            dispatch(setCurrentMarket(currentMarket));
+            history.push(`/trading/${currentMarket.id}`);
+        }
+    };
 
-			return require('../../../node_modules/cryptocurrency-icons/svg/color/generic.svg');
-		}
-	};
+    const MarketChart = (data: any, marketID: string) => {
 
-	const history = useHistory();
+        const market = markets.find(market => market.base_unit.toLowerCase() === marketID.split('/')[0].toLowerCase());
+        const getTickerValue = (cMarket: Market, tickers: { [key: string]: Ticker }) => {
+            const defaultTicker = { amount: 0, low: 0, last: 0, high: 0, volume: 0, open: 0, price_change_percent: '+0.00%' };
 
-	const MarketChart = (data: any, marketIdParam: string) => {
-		const market = markets.find(
-			marketParam => marketParam.base_unit.toLowerCase() === marketIdParam.split('/')[0].toLowerCase(),
-		);
-		const getTickerValue = React.useCallback((cMarket: Market, tickers: { [key: string]: Ticker }) => {
-			return tickers[cMarket.id] || defaultTicker;
-		}, []);
+            return tickers[cMarket.id] || defaultTicker;
+        };
 
-		const currentTicker = (currentMarket && getTickerValue(currentMarket, marketTickers)) || { volume: 0 };
-		// console.log(currentTicker);
+        const currentTicker = currentMarket && getTickerValue(currentMarket, marketTickers) || { volume: 0 };
 
-		if (market) {
-			const marketID = market.name.toUpperCase();
-			const baseCurrency = marketID.split('/')[0];
-			const quoteCurrency = marketID.split('/')[1];
-			const last = Number((marketTickers[market.id] || defaultTicker).last);
-			const open = Number((marketTickers[market.id] || defaultTicker).open);
-			const price_change_percent = (marketTickers[market.id] || defaultTicker).price_change_percent;
-			const volume = Number((currentTicker[market.id] || defaultTicker).volume);
-			const change = +last - +open;
-			// color
-			const marketChangeColor = +(change || 0) < 0 ? 'var(--system-red)' : 'var(--system-green)';
+        if (market) {
+            const marketID = market.name.toUpperCase();
+            const baseCurrency = marketID.split('/')[0];
+            const quoteCurrency = marketID.split('/')[1];
+            const last = Number((marketTickers[market.id] || defaultTicker).last);
+            const open = Number((marketTickers[market.id] || defaultTicker).open);
+            const price_change_percent = (marketTickers[market.id] || defaultTicker).price_change_percent;
+            const volume = Number((currentTicker[market.id] || defaultTicker).volume);
+            const change = (+last - +open);
+            const marketChangeColor = +(change || 0) < 0 ? "var(--system-red)" : "var(--system-green)";
+            return (
+                <MarketChartItem>
+                    <div className="container" onClick={() => handleRedirectToTrading(market.id)}>
+                        <div className="row">
+                            <div className="col-12 d-flex justify-content-between">
+                                <div>
+                                    <img width="30px" height="30px" src={findIcon(baseCurrency)} alt={baseCurrency} />
+                                    <span style={{ fontSize: '1.2rem', margin: '5px' }} className="text-white">{marketID.toUpperCase()}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mt-1">
+                            <div className="col-6 d-flex justify-content-start align-items-center">
+                                <span style={{ marginLeft: '5px', fontSize: '1.4rem', color: '#fff' }}>{last.toFixed(6)}</span>
+                            </div>
 
-			return (
-				<MarketChartItem>
-					<div
-						className="container"
-						onClick={() => history.push(`/trading/${baseCurrency.toLowerCase() + quoteCurrency.toLowerCase()}`)}
-					>
-						<div className="row">
-							<div className="col-12 d-flex justify-content-between">
-								<div>
-									<img width="30px" height="30px" src={findIcon(baseCurrency)} alt={baseCurrency} />
-									<span style={{ fontSize: '1.2rem', margin: '5px' }} className="text-white">
-										{marketID.toUpperCase()}
-									</span>
-								</div>
-							</div>
-						</div>
-						<div className="row mt-1">
-							<div className="col-6 d-flex justify-content-start align-items-center">
-								<span style={{ marginLeft: '5px', fontSize: '1.4rem', color: '#fff' }}>{last.toFixed(6)}</span>
-							</div>
+                            <div className="col-6 d-flex justify-content-end align-items-center" >
+                                <ResponsiveContainer ani width='100%' aspect={2.5 / 1.0}>
+                                    <AreaChart
+                                        width={200}
+                                        height={60}
+                                        data={data}
+                                        margin={{
+                                            top: 5,
+                                            right: 0,
+                                            left: 0,
+                                            bottom: 5
+                                        }}
+                                    >
+                                        <Area isAnimationActive={false} type="monotone" dataKey="pv" stroke="#fff" fill="#7d82b8" />
+                                    </AreaChart>
+                                </ResponsiveContainer>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 d-flex justify-content: center">
+                                <span style={{ marginRight: '5px', color: marketChangeColor, fontWeight: 'bold' }}>{price_change_percent}</span>
+                                <div className="ml-2" >
+                                    <span style={{ color: '#ced4da' }}>Volume:</span>
+                                    <span className="ml-2" style={{ marginRight: '5px', color: '#ced4da', fontWeight: 'bold' }}>{volume}</span>
+                                    <span style={{ color: '#ced4da' }}>{quoteCurrency.toUpperCase()}</span>
+                                </div>
+                            </div>
 
-							<div className="col-6 d-flex justify-content-end align-items-center">
-								<ResponsiveContainer ani width="100%" aspect={2.5 / 1}>
-									<AreaChart
-										width={200}
-										height={60}
-										data={data}
-										margin={{
-											top: 5,
-											right: 0,
-											left: 0,
-											bottom: 5,
-										}}
-									>
-										<Area
-											isAnimationActive={false}
-											type="monotone"
-											dataKey="pv"
-											stroke="#fff"
-											fill="#7d82b8"
-										/>
-									</AreaChart>
-								</ResponsiveContainer>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-12 d-flex justify-content: center">
-								<span style={{ marginRight: '5px', color: marketChangeColor, fontWeight: 'bold' }}>
-									{price_change_percent}
-								</span>
-								<div className="ml-2">
-									<span style={{ color: '#ced4da' }}>Volume:</span>
-									<span className="ml-2" style={{ marginRight: '5px', color: '#ced4da', fontWeight: 'bold' }}>
-										{volume}
-									</span>
-									<span style={{ color: '#ced4da' }}>{quoteCurrency.toUpperCase()}</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</MarketChartItem>
-			);
-		}
+                        </div>
+                    </div>
+                </MarketChartItem>
+            );
+        }
+        return '';
+    }
 
-		return '';
-	};
-
-	const renderChart = () => {
-		return (
-			<ChartWrap>
-				<div className="container" style={{ backgroundColor: 'transparent', padding: '25px 0px', borderRadius: '1rem' }}>
-					<div className="row">
-						<div className="col-lg-3 col-md-6 mb-2">{MarketChart(KlineState1, marketIds[0])}</div>
-						<div className="col-lg-3 col-md-6">{MarketChart(KlineState2, marketIds[1])}</div>
-						<div className="col-lg-3 col-md-6">{MarketChart(KlineState3, marketIds[2])}</div>
-						<div className="col-lg-3 col-md-6">{MarketChart(KlineState4, marketIds[3])}</div>
-					</div>
-				</div>
-			</ChartWrap>
-		);
-	};
-
-	return <React.Fragment>{renderChart()}</React.Fragment>;
-};
+    const renderChart = () => {
+        return (
+            <ChartWrap>
+                <div className="container" style={{ backgroundColor: 'transparent', padding: '25px 0px', borderRadius: '1rem' }}>
+                    <div className="row">
+                        {kLinesState.map((kline, i) => (
+                            < div className="col-lg-3 col-md-6 mb-2" key={i} >
+                                { MarketChart(kline, marketNames[i])}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </ChartWrap >
+        );
+    }
+    return (
+        <React.Fragment>
+            {renderChart()}
+        </React.Fragment>
+    )
+}
