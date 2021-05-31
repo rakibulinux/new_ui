e57cc8eeaba7d179822a41c2f5521fd583c36198
Huỳnh Văn Phước <47907938+phuocvn@users.noreply.github.com>
2021-05-31 11:07:43 +0700

    Fix: Deposit and Withdraw History Mobile

--- src/mobile/components/WithdrawHistoryTable/index.tsx
+++ src/mobile/components/WithdrawHistoryTable/index.tsx
@@ -1,39 +1,27 @@
 import * as React from 'react';
 import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
-import { Pagination, Table } from '../../../components';
+import { Table } from '../../../components';
 import { DEFAULT_CCY_PRECISION } from '../../../constants';
 import { localeDate } from '../../../helpers';
 import { useCurrenciesFetch, useHistoryFetch, useWalletsFetch } from '../../../hooks';
-import { RootState, selectCurrentPage, selectLastElemIndex, selectNextPageExists } from '../../../modules';
 import { selectCurrencies } from '../../../modules/public/currencies';
-import { selectFirstElemIndex, selectHistory } from '../../../modules/user/history';
+import { selectHistory } from '../../../modules/user/history';
 import { selectWallets } from '../../../modules/user/wallets';
 import { RowItem } from './Rowitem';
 
 const DEFAULT_LIMIT = 6;
 
 const WithdrawHistoryTable = (props: any) => {
-	const [currentPage, setCurrentPage] = React.useState(0);
 	const intl = useIntl();
-	const page = useSelector(selectCurrentPage);
 	const list = useSelector(selectHistory);
 	const wallets = useSelector(selectWallets);
 	const currencies = useSelector(selectCurrencies);
-	const firstElemIndex = useSelector((state: RootState) => selectFirstElemIndex(state, DEFAULT_LIMIT));
-	const lastElemIndex = useSelector((state: RootState) => selectLastElemIndex(state, DEFAULT_LIMIT));
-	const nextPageExists = useSelector((state: RootState) => selectNextPageExists(state, DEFAULT_LIMIT));
 
 	useWalletsFetch();
 	useCurrenciesFetch();
-	useHistoryFetch({ type: props.type, currency: props.currency, limit: DEFAULT_LIMIT, page: currentPage });
+	useHistoryFetch({ type: props.type, currency: props.currency, limit: DEFAULT_LIMIT, page: 0 });
 
-	const onClickPrevPage = () => {
-		setCurrentPage(Number(page) - 1);
-	};
-	const onClickNextPage = () => {
-		setCurrentPage(Number(page) + 1);
-	};
 	const formatTxState = (tx: string, confirmations?: number, minConfirmations?: number) => {
 		const process = require('../../../assets/status/wait.svg');
 		const fail = require('../../../assets/status/fail.svg');
@@ -66,19 +54,22 @@ const WithdrawHistoryTable = (props: any) => {
 			return [[intl.formatMessage({ id: 'page.noDataToShow' }), '', '']];
 		}
 
-		return list
+		const histories = list
+			.filter((history: any) => history.currency === currency)
 			.sort((a, b) => {
 				return localeDate(a.created_at, 'fullDate') > localeDate(b.created_at, 'fullDate') ? -1 : 1;
 			})
-			.map((item: any, index) => {
+			.map((item: any) => {
 				const amount = 'amount' in item ? Number(item.amount) : Number(item.price) * Number(item.volume);
-				const confirmations = type === 'withdraws' && item.confirmations;
+				const confirmations = type === 'deposits' && item.confirmations;
 				const itemCurrency = currencies && currencies.find(cur => cur.id === currency);
 				const minConfirmations = itemCurrency && itemCurrency.min_confirmations;
 				const state = 'state' in item ? formatTxState(item.state, confirmations, minConfirmations) : '';
 
 				return [<RowItem amount={amount} fixed={fixed} currency={currency} createdAt={item.created_at} />, state];
 			});
+
+		return histories.length > 0 ? histories : [[]];
 	};
 	const mapRows = row => {
 		return <div className="cr-mobile-history-table__row">{row}</div>;
@@ -89,14 +80,6 @@ const WithdrawHistoryTable = (props: any) => {
 	return (
 		<div className="cr-mobile-history-table">
 			<Table data={tableData} />
-			<Pagination
-				firstElemIndex={firstElemIndex}
-				lastElemIndex={lastElemIndex}
-				page={page}
-				nextPageExists={nextPageExists}
-				onClickPrevPage={onClickPrevPage}
-				onClickNextPage={onClickNextPage}
-			/>
 		</div>
 	);
 };
