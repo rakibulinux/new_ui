ce91bead7186b28f2f2e96c4555d5110014a4fbd
LeThanhDat <thanhdat@LeThanhDats-MacBook-Pro.local>
2021-06-09 03:55:24 +0700

    Fix: add csrf-token

--- src/modules/plugins/vote/sagas/voteSaga.ts
+++ src/modules/plugins/vote/sagas/voteSaga.ts
@@ -1,16 +1,17 @@
 import { API, RequestOptions } from 'api';
+import { getCsrfToken } from 'helpers';
 import { alertPush } from 'modules/public/alert';
 import { stringify } from 'querystring';
 import { call, put } from 'redux-saga/effects';
 import { VoteDonateCreate, voteDonateData, VoteListData, voteListData, voteListError, VoteListFetch } from '../actions';
 
-const voteOptions: RequestOptions = {
-	apiVersion: 'sunshine',
+const createOptions = (csrfToken?: string): RequestOptions => {
+	return { apiVersion: 'sunshine', headers: { 'X-CSRF-Token': csrfToken } };
 };
 
 export function* votesFetchSaga(actions: VoteListFetch) {
 	try {
-		const voteList = yield call(API.get(voteOptions), `/public/vote/list?${stringify(actions.payload)}`);
+		const voteList = yield call(API.get(createOptions()), `/public/vote/list?${stringify(actions.payload)}`);
 		yield put(voteListData(voteList as VoteListData['payload']));
 	} catch (error) {
 		yield put(voteListError(error));
@@ -20,7 +21,7 @@ export function* votesFetchSaga(actions: VoteListFetch) {
 
 export function* voteDonateCreateSaga(actions: VoteDonateCreate) {
 	try {
-		yield call(API.post(voteOptions), `private/vote/donate`, actions.payload);
+		yield call(API.post(createOptions(getCsrfToken())), `private/vote/donate`, actions.payload);
 		yield put(voteDonateData(actions.payload));
 		yield put(alertPush({ message: ['Vote success'], type: 'success' }));
 	} catch (error) {
