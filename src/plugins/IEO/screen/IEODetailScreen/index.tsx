10d041c090be3126d2fb743f7a7b8f1bf47b63bb
Phuoc <ripker1805@gmail.com>
2021-06-21 14:11:02 +0700

    change information of IEO <IEODetai<

--- src/plugins/IEO/screen/IEODetailScreen/index.tsx
+++ src/plugins/IEO/screen/IEODetailScreen/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
 export const IEODetailScreen = () => {
+	// const listCoins = ['prob', 'xrp', 'btc', 'eth', 'xrp'];
 	return (
 		<div id="ieo-detail-screen">
 			<h3>IEO</h3>
 			<button id="ioe-detail-screen__return-list">Return to Lists</button>
+			<div id="ioe-detail-screen_container row">
+				<div className="ieo-info col-6"></div>
+				<div className="ieo-buy col-6"></div>
+			</div>
 		</div>
 	);
 };
