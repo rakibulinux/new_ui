48a4d54fa781ca47caa582f120c7bdec454fbec5
ThanhDat <thanhdat@ThanhDats-MacBook-Pro.local>
2021-06-24 15:21:16 +0700

    Fix: color input search and restructure code

--- src/mobile/screens/NewWalletsScreen/index.tsx
+++ src/mobile/screens/NewWalletsScreen/index.tsx
@@ -1,12 +1,12 @@
 import { Empty } from 'antd';
+import { ConvertUsd } from 'components';
 import { calcWalletsData } from 'helpers';
 import { useAllChildCurrenciesFetch, useDocumentTitle, useWalletsFetch } from 'hooks';
 import { SearchIcon } from 'mobile/assets/icons';
 import React, { useState } from 'react';
 import { DebounceInput } from 'react-debounce-input';
 import { useSelector } from 'react-redux';
-import { selectAllChildCurrencies, selectWallets } from '../../../modules';
-import { NewWalletListItem } from './NewWalletListItem';
+import { selectAllChildCurrencies, selectWallets, Wallet } from '../../../modules';
 
 export const NewWalletsMobileScreen = () => {
 	useDocumentTitle('Wallets');
@@ -20,17 +20,48 @@ export const NewWalletsMobileScreen = () => {
 	const [searchString, setSearchString] = useState<string>('');
 	const [hideSmallBalance, setHideSmallBalance] = useState<boolean>(false);
 
-	const data = calcWalletsData(wallets, allChildCurrencies)
-		.filter(({currency, total}) =>
-			currency.includes(searchString.toLowerCase().trim())
-			&& (hideSmallBalance ? Number(total) > 0 : true),
-		);
+	const data = calcWalletsData(wallets, allChildCurrencies).filter(
+		({ currency, total }) =>
+			currency.includes(searchString.toLowerCase().trim()) && (hideSmallBalance ? Number(total) > 0 : true),
+	);
+
+	const renderWalletList = (walletsParam: Wallet[]) => {
+		return walletsParam.map(wallet => (
+			<div className="td-mobile-wallets__list__item" key={wallet.currency}>
+				<div className="td-mobile-wallets__list__item__top">
+					<img className="td-mobile-wallets__list__item__top__icon" src={wallet.iconUrl} alt={wallet.name} />
+					<span className="td-mobile-wallets__list__item__top__symbol">{wallet.currency.toUpperCase()}</span>
+				</div>
+				<div className="td-mobile-wallets__list__item__bottom">
+					<div>
+						<span className="td-mobile-wallets__list__item__bottom__text">Estimation</span>
+						<span className="td-mobile-wallets__list__item__bottom__number">
+							≈${' '}
+							<ConvertUsd
+								value={Number(wallet.total)}
+								symbol={wallet.currency}
+								precision={4}
+								defaultValue={'0.00'}
+							/>
+						</span>
+					</div>
+					<div>
+						<span className="td-mobile-wallets__list__item__bottom__text">Total</span>
+						<span className="td-mobile-wallets__list__item__bottom__number">{wallet.total}</span>
+					</div>
+					<div>
+						<span className="td-mobile-wallets__list__item__bottom__text">Availible</span>
+						<span className="td-mobile-wallets__list__item__bottom__number">{wallet.balance}</span>
+					</div>
+				</div>
+			</div>
+		));
+	};
 
 	return (
 		<div className="td-mobile-wallets">
-			<div className="td-mobile-wallets__header" >
-
-				<label className="td-mobile-wallets__header__search-box" htmlFor="td-mobile-wallets-search-box" >
+			<div className="td-mobile-wallets__header">
+				<label className="td-mobile-wallets__header__search-box" htmlFor="td-mobile-wallets-search-box">
 					<SearchIcon className="td-mobile-wallets__header__search-box__icon" />
 					<DebounceInput
 						id="td-mobile-wallets-search-box"
@@ -40,7 +71,7 @@ export const NewWalletsMobileScreen = () => {
 					/>
 				</label>
 
-				<div className="td-mobile-wallets__header__toggle" >
+				<div className="td-mobile-wallets__header__toggle">
 					<span className="td-mobile-wallets__header__toggle__text">Hide small balance</span>
 					<label className="td-mobile-wallets__header__toggle__checkbox" htmlFor="td-mobile-wallet-hide-small-balance">
 						<input
@@ -53,13 +84,7 @@ export const NewWalletsMobileScreen = () => {
 					</label>
 				</div>
 			</div>
-			<div className="td-mobile-wallets__list" >
-			{
-				data.length === 0
-				? <Empty />
-				: data.map(wallet => <NewWalletListItem key={wallet.currency} {...wallet} />)
-			}
-			</div>
+			<div className="td-mobile-wallets__list">{data.length === 0 ? <Empty /> : renderWalletList(data)}</div>
 		</div>
 	);
 };
