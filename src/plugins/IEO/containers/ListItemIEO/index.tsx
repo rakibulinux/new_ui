87bb469c7def5d68cddb9252109c4af342f20105
Phuoc <ripker1805@gmail.com>
2021-06-29 07:38:17 +0700

    is Empty IEO

--- src/plugins/IEO/containers/ListItemIEO/index.tsx
+++ src/plugins/IEO/containers/ListItemIEO/index.tsx
@@ -7,26 +7,38 @@ interface ListItemIEOProps {
 }
 export const ListItemIEO: React.FC<ListItemIEOProps> = props => {
 	const { IEOList } = props;
-
+	const EmptyComponent = () => {
+		return (
+			<div className="col-12 d-flex justify-content-center">
+				<img
+					src="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+					style={{ marginTop: '3rem' }}
+					alt="empty"
+				/>
+			</div>
+		);
+	};
 	return (
 		<div id="ioe-listing-screen-ieos" className="row mt-5">
-			{IEOList.map((item, index) => {
-				return (
-					<div className="col-md-6 col-lg-4 col-xl-3" style={{ padding: '10px 10px' }}>
-						<IEOItem
-							type={item.type}
-							currencyId={item.currency_id}
-							startDate={item.start_date}
-							endDate={item.end_date}
-							currencyAvailable={item.currency_available}
-							description={item.description}
-							bonus={item.bonus}
-							key={index}
-							id={item.id}
-						/>
-					</div>
-				);
-			})}
+			{!IEOList.length
+				? EmptyComponent()
+				: IEOList.map((item, index) => {
+						return (
+							<div className="col-md-6 col-lg-4 col-xl-3" style={{ padding: '10px 10px' }}>
+								<IEOItem
+									type={item.type}
+									currencyId={item.currency_id}
+									startDate={item.start_date}
+									endDate={item.end_date}
+									currencyAvailable={item.currency_available}
+									description={item.description}
+									bonus={item.bonus}
+									key={index}
+									id={item.id}
+								/>
+							</div>
+						);
+				  })}
 		</div>
 	);
 };
