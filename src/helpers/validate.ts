ecfb9f9a9a815649b54da1e67c528d3d23ae0796
Trung Luc <trungluc217@gmail.com>
2021-07-03 19:29:51 -0700

    Fix: re-export

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,10 +1,10 @@
-export const EMAIL_REGEX = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
-export const PASSWORD_REGEX = /^(?=.{8,})/;
+export const EMAIL_PARTTERN = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+export const PASSWORD_PARTTERN = /^(?=.{8,})/;
 
 export const isEmail = (email: string): boolean => {
-	return EMAIL_REGEX.test(email);
+	return EMAIL_PARTTERN.test(email);
 };
 
 export const isValidPassword = (pass: string): boolean => {
-	return PASSWORD_REGEX.test(pass);
+	return PASSWORD_PARTTERN.test(pass);
 };
