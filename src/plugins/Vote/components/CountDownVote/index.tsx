96268e813cfa9126f4fc861b386009c64bb8f6d3
Lê Thành Đạt <lthanhdat10x@gmail.com>
2021-07-05 16:39:50 +0700

    Fix: change ui vote

--- src/plugins/Vote/components/CountDownVote/index.tsx
+++ src/plugins/Vote/components/CountDownVote/index.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames';
 import { selectVoteListInfoRound } from 'modules';
 import moment from 'moment';
 import * as React from 'react';
@@ -5,33 +6,63 @@ import Countdown, { CountdownRendererFn } from 'react-countdown';
 import isEqual from 'react-fast-compare';
 import { useSelector } from 'react-redux';
 
+const rendererCountDown: CountdownRendererFn = ({ days, hours, minutes, seconds, completed }) => {
+	const format = (value: number) => (value.toString().length === 1 ? `0${value}` : value.toString());
+	const timeElm = (
+		<div className="pg-vote__cpn__countdown__time-remaining text-center">
+			<span>
+				{format(days)}
+				<i>Days</i>
+			</span>
+			<span>
+				{format(hours)}
+				<i>Hours</i>
+			</span>
+			<span>
+				{format(minutes)}
+				<i>Minutes</i>
+			</span>
+			<span>
+				{format(seconds)} <i>Seconds</i>
+			</span>
+		</div>
+	);
+	if (completed) {
+		return (
+			<React.Fragment>
+				<div className="pg-vote__cpn__countdown__done text-center">
+					The vote has ended. Thank you for your participation!
+				</div>
+				{timeElm}
+			</React.Fragment>
+		);
+	} else {
+		return timeElm;
+	}
+};
+
 // tslint:disable-next-line: no-empty-interface
 interface CountDownVoteProps {}
 
 const CountDownVoteCpn: React.FC<CountDownVoteProps> = ({}) => {
 	const infoRound = useSelector(selectVoteListInfoRound, isEqual);
-	const diffNum = infoRound ? moment(infoRound.ended_at).diff(moment(infoRound.current_time)) : 0;
-
-	const rendererCountDown: CountdownRendererFn = ({ days, hours, minutes, seconds, completed }) => {
-		if (completed) {
-			return <div className="pg-vote__cpn__countdown__done text-center">Waiting for the next round!</div>;
-		} else {
-			return (
-				<React.Fragment>
-					<div className="pg-vote__cpn__countdown__time-remaining text-center">
-						Days left {days}
-						<br />
-						Time : {hours}:{minutes}:{seconds}
-					</div>
-				</React.Fragment>
-			);
-		}
-	};
+	const [diffNum, setDiffNum] = React.useState(0);
+	React.useEffect(() => {
+		setDiffNum(infoRound ? moment(infoRound.ended_at).diff(moment(infoRound.current_time)) : 0);
+	}, [infoRound]);
 
 	return (
-		<div className="pg-vote__cpn__countdown d-flex">
-			<div className="pg-vote--border pg-vote__cpn__countdown__count">
-				<Countdown date={Date.now() + diffNum} renderer={rendererCountDown} />
+		<div className="pg-vote__cpn__countdown">
+			<div
+				className={classnames('pg-vote--border pg-vote__cpn__countdown__count', {
+					'mb-3': infoRound && infoRound.coin_id,
+				})}
+			>
+				<Countdown
+					intervalDelay={1000}
+					date={diffNum ? Date.now() + diffNum : Date.now() + 10000}
+					renderer={rendererCountDown}
+				/>
 			</div>
 			{infoRound && infoRound.coin_id && (
 				<div className="pg-vote--border pg-vote__cpn__countdown__last-win text-center">
